879fe719a254503e5a51b45ad304ec2c95dec5c6
Will0301 <willian.f.w@hotmail.com>
2026-05-09 22:21:47 -0300

    fix: output

--- infra/output.tf
+++ infra/output.tf
@@ -1,19 +1,3 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
-}
-
-output "lambda_function_name" {
-  value = aws_lambda_function.app.function_name
-}
-
-output "lambda_alias_name" {
-  value = aws_lambda_alias.live.name
-}
-
-output "function_url" {
-  value = aws_lambda_function_url.app_url.function_url
-}
-
 output "database_instance_id" {
   value = aws_instance.database.id
 }
